72700ac055fdc3b7b6c9032ebc524039e1c149a1
JsNox <joel.silos@hotmail.com>
2025-03-03 13:24:10 -0600

    helper consumo

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -3,13 +3,13 @@ import type { MenuItem } from "../types"
 
 type MenuItemProps = {
     item: MenuItem,
-    addItem: () => void
+    addItem: (item: MenuItem) => void
 }
 
 export default function MenuItem({ item, addItem }: MenuItemProps) {
     return (
         <button className=" border-2 border-red-600 hover:bg-red-200 w-full p-3 flex justify-between"
-        onClick={() => addItem()}
+        onClick={() => addItem(item)}
         >
             <p>{item.name}</p>
             <p className="font-black">${item.price}</p>
