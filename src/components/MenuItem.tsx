44d5b123c6e413cbcf265287eeacefced1609f25
JsNox <joel.silos@hotmail.com>
2025-03-02 22:06:18 -0600

    customhook

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,11 +1,16 @@
 import type { MenuItem } from "../types"
+
+
 type MenuItemProps = {
-    item: MenuItem
+    item: MenuItem,
+    addItem: () => void
 }
 
-export default function MenuItem({ item }: MenuItemProps) {
+export default function MenuItem({ item, addItem }: MenuItemProps) {
     return (
-        <button className=" border-2 border-red-600 hover:bg-red-200 w-full p-3 flex justify-between">
+        <button className=" border-2 border-red-600 hover:bg-red-200 w-full p-3 flex justify-between"
+        onClick={() => addItem()}
+        >
             <p>{item.name}</p>
             <p className="font-black">${item.price}</p>
         </ button>
