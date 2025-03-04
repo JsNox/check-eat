66d9b643f368a9b07a3a1934867eb322525b1d3b
JsNox <joel.silos@hotmail.com>
2025-03-03 23:06:09 -0600

    order & tip

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -4,6 +4,7 @@ import type { MenuItem, OrderItem } from "../types"
 export default function useOrder() {
 
     const [order, setOrder] = useState<OrderItem[]>([])
+    const [tip, setTip] = useState(0)
 
     const addItem = (item: MenuItem) => {
 
@@ -26,13 +27,18 @@ export default function useOrder() {
         setOrder(order.filter( item => item.id != id))
     }
 
-
-
+    const placeOrder = () => {
+        setOrder([])
+        setTip(0)
+    }
 
     return {
         order,
+        tip,
+        setTip,
         addItem,
-        removeItem
+        removeItem,
+        placeOrder
 
     }
 }
